b1d21342aa7253ba1c10a2a20cfa7cd40cf1fff6
Wlodek Bzyl <matwb@ug.edu.pl>
2013-12-14 19:19:00 +0100

    pretyprint programs code: from sinatra-blog

--- lib/c-exercises.rb
+++ lib/c-exercises.rb
@@ -31,10 +31,55 @@ module WB
       erb(markdown(:"#{params[:section]}"))
     end
 
+
+    # prettyprint file contents located in 'doc' directory
+
+    get %r{^([-_\w\/]+)\/([-_\w]+)\.((\w{1,4})(\.\w{1,4})?)$} do
+
+      translate = { # to coderay syntax names
+        'html.erb' => 'erb',
+        'text.erb' => 'text',
+        'erb' => 'erb',
+        'rb' => 'ruby',
+        'ru' => 'ruby',
+        'js' => 'javascript',
+        'yml' => 'yaml',
+        'sql' => 'sql',
+        'json' => 'json',
+        'c' => 'c',
+        'sh' => 'bash'
+      }
+
+      content_type 'text/html', :charset => 'utf-8'
+
+      dirname = params[:captures][0]
+      name = params[:captures][1]
+      extname = params[:captures][2]
+      filename = name + "." + extname
+
+      @title = dirname.split('/').join(' » ')
+
+      @filename = File.expand_path(File.join(settings.root, 'public/doc', dirname, filename))
+      # puts STDERR, @filename
+
+      lang = translate[extname] || 'plain_text'
+
+      if File.exists?(@filename) && File.readable?(@filename)
+        content = "<h1>#{filename}</h1>"
+        content += "<pre><code>:::#{lang}\n#{escape_html(File.read @filename)}</code></pre>"
+      else
+        content  = "<h2> oops! couldn't find <em>#{filename}</em></h2>"
+      end
+
+      erb content, :layout => :code
+    end
+
+    not_found do
+      'This is nowhere to be found.'
+    end
+
     error do
-      e = request.env['sinatra.error']
-      Kernel.puts e.backtrace.join("\n")
-      'Application error'
+      'Sorry there was a nasty error – ' + env['sinatra.error'].name
     end
 
   end
